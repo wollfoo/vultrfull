fa10f39ae43a5ed954759953eb8b5f8a60fef804
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2020-08-17 11:06:22 +0700

    Update loki-full.sh

--- loki-full.sh
+++ loki-full.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-VERSION=5.11.1
+VERSION=6.3.1
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 apt-get install -y git wget screen
