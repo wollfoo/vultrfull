e8393ae5d62512985ecb2902d0c7bd95483a71c6
wollfoo <nhac.tien1988@gmail.com>
2023-12-30 17:52:06 +0700

    Update ubuntu_tor.sh

--- ubuntu_tor.sh
+++ ubuntu_tor.sh
@@ -8,6 +8,6 @@ sudo -i
 apt-get update
 apt-get install -y git wget
 rm -rf loki-full.sh
-wget https://raw.githubusercontent.com/wollfoo/orcfull/master/zepfull.sh
+wget https://raw.githubusercontent.com/wollfoo/vultrfull/master/zepfull.sh
 chmod 777 zepfull.sh
 ./zepfull.sh
